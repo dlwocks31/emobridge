66b0fce7e852bf886a5b4ee14055fc3660daa613
HHCHU <ded06031@snu.ac.kr>
2023-07-29 14:03:41 +0900

    :art: Emoji 스타일링 더 적용

--- components/Emoji.tsx
+++ components/Emoji.tsx
@@ -1,4 +1,3 @@
-
 export const Emoji = ({
     emoji,
 }: {
@@ -12,7 +11,7 @@ export const Emoji = ({
     border: 1px solid rgba(0, 0, 0, 0.15);
     background: #FFF;
     box-shadow: -0.6916777491569519px -2.075033187866211px 0px 0px rgba(0, 0, 0, 0.10) inset, 0px 0.6916777491569519px 2.075033187866211px 0px rgba(0, 0, 0, 0.20); */
-    return <div className="flex h-14 w-14 items-center justify-center rounded-full bg-white flex-shrink-0 border border-black border-opacity-10">
+    return <div className="flex h-8 w-8 items-center justify-center rounded-full bg-white flex-shrink-0 border border-black border-opacity-10 shadow">
         {emoji}
     </div>
 }
