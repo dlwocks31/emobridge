90095c08db1150a4f4f2af82bb17a38381ebc359
Jaechan <dlwocks31@gmail.com>
2023-07-30 09:44:09 +0900

    코드 정리

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,21 @@
-import { AuthForm } from "./AuthForm";
+"use client";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
-export default function App() {
-  return <AuthForm />;
+export default function Login() {
+  const supabase = createClientComponentClient();
+  const host = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : `http://localhost:3000`;
+  return (
+    <Auth
+      supabaseClient={supabase}
+      appearance={{ theme: ThemeSupa }}
+      showLinks={false}
+      providers={["google"]}
+      redirectTo={`${host}/auth/callback`}
+      onlyThirdPartyProviders={true}
+    />
+  );
 }
