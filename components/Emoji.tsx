8d40f701b581f3df78649d9d9eaf7ae677c7128c
HHCHU <ded06031@snu.ac.kr>
2023-08-05 17:44:44 +0900

    :art: Emoji image update

--- components/Emoji.tsx
+++ components/Emoji.tsx
@@ -7,6 +7,6 @@ export const Emoji = ({
 }) => {
     // className="flex h-8 w-8 items-center justify-center rounded-full bg-white flex-shrink-0 border border-black border-opacity-10 shadow"
     return <div>
-        <Image src={emoji} alt="me" width="64" height="64" />
+        <Image src={emoji} alt="me" width="35" height="35" />
     </div>
 }
