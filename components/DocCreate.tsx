cbac21a0f02f14dab56b79607d72c31252e53be0
Jaechan <dlwocks31@gmail.com>
2023-09-10 12:14:35 +0900

    문서제목이 있을 때만 추가가능하도록 함

--- components/DocCreate.tsx
+++ components/DocCreate.tsx
@@ -4,12 +4,14 @@ import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Database } from "../database.types";
+import { cn } from "../utils/cn";
 
 export function DocCreate({ courseId }: { courseId: number }) {
   const supabase = createClientComponentClient<Database>();
   const [docName, setDocName] = useState("");
   const router = useRouter();
   const createDocument = async () => {
+    if (!docName) return;
     const { error } = await supabase
       .from("documents")
       .insert({ name: docName, courseId });
@@ -30,7 +32,12 @@ export function DocCreate({ courseId }: { courseId: number }) {
         value={docName}
         onChange={(e) => setDocName(e.target.value)}
       />
-      <div className="btn" onClick={createDocument}>
+      <div
+        className={cn("btn", {
+          "btn-disabled": !docName,
+        })}
+        onClick={createDocument}
+      >
         문서 생성
       </div>
     </div>
