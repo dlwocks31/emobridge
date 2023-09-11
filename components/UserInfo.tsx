5995e9859eefd62c06c8aefab4beff6e6a3875db
Jaechan <dlwocks31@gmail.com>
2023-09-11 17:32:54 +0900

    로그아웃 버튼 dropdown으로 구현

--- components/UserInfo.tsx
+++ components/UserInfo.tsx
@@ -9,20 +9,39 @@ import { useRouter } from "next/navigation";
 export const UserInfo = ({ user }: { user: User | null }) => {
   const supabase = createClientComponentClient();
   const router = useRouter();
-  return (
-    <div
-      className="tooltip tooltip-bottom"
-      data-tip={user ? `${user.email} / 로그아웃` : "로그인"}
-      onClick={async () => {
-        if (user) {
-          await supabase.auth.signOut();
-          router.refresh();
-        } else {
-          router.push("/auth/login");
-        }
-      }}
-    >
-      <Image src="/user.png" alt="user" width="24" height="24" />
-    </div>
-  );
+  if (user) {
+    return (
+      <div className="dropdown dropdown-bottom dropdown-end dropdown-hover">
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <li>
+            <a>{user.email}</a>
+          </li>
+          <li
+            onClick={async () => {
+              await supabase.auth.signOut();
+              router.refresh();
+            }}
+          >
+            <a>로그아웃</a>
+          </li>
+        </ul>
+        <label tabIndex={0}>
+          <Image src="/user.png" alt="user" width="24" height="24" />
+        </label>
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="tooltip tooltip-bottom"
+        data-tip="로그인"
+        onClick={() => router.push("/auth/login")}
+      >
+        <Image src="/user.png" alt="user" width="24" height="24" />
+      </div>
+    );
+  }
 };
