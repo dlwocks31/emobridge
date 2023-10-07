0abe34e9b0a005cb92c206f0a665524a56856afa
Jaechan <dlwocks31@gmail.com>
2023-10-08 01:29:44 +0900

    feat: 로그 다운로드에 새로 추가한 필드들 다운로드 추가

--- app/feedbacker/doc/[id]/logs/route.ts
+++ app/feedbacker/doc/[id]/logs/route.ts
@@ -5,8 +5,8 @@ import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import Papa from "papaparse";
 
-// download csv
 export async function GET(request: NextRequest, response: NextResponse) {
   const docId = +request.url.split("/")[5];
   const supabase = createRouteHandlerClient<Database>({ cookies });
@@ -15,7 +15,9 @@ export async function GET(request: NextRequest, response: NextResponse) {
     "documentId",
     "logType",
     "emojiType",
+    "emojiClass",
     "targetBlockId",
+    "blockContent",
     "createdBy",
     "createdAt",
   ] as const;
@@ -25,22 +27,32 @@ export async function GET(request: NextRequest, response: NextResponse) {
     .select("*")
     .eq("documentId", docId);
 
-  const headers = columns.join(",");
-  const values = data
-    ?.map((row) => {
-      return columns.map((column) => {
-        return row[column];
-      });
-    })
-    .join("\n");
+  if (error) {
+    // Handle the error appropriately
+    throw error;
+  }
 
-  const csv = `${headers}\n${values}`;
+  const csvData = data?.map((row) => {
+    return columns.reduce((acc, column) => {
+      acc[column] = row[column];
+      return acc;
+    }, {} as any);
+  });
+
+  const csv = Papa.unparse({
+    fields: [...columns],
+    data: csvData,
+  });
+
+  const BOM = "\uFEFF";
+  const csvWithBOM = BOM + csv; // Add BOM to the beginning of the csv string to make it work on Excel (encoding issue)
 
   dayjs.extend(utc);
   dayjs.extend(timezone);
   dayjs.tz.setDefault("Asia/Seoul");
   const fileName = `logs-${docId}-${dayjs().format("YYYYMMDD-HHmmss")}.csv`;
-  return new Response(csv, {
+
+  return new Response(csvWithBOM, {
     headers: {
       "content-type": "text/csv; charset=utf-8",
       "content-disposition": `attachment; filename="${fileName}"`,
